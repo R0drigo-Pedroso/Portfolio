50c5d8025581891f3b9575468457946f4d809222
Rodrigo Pedroso <rodrigo.pedroso@live.com>
2023-06-10 18:59:47 -0300

    modificação js

--- js/animacaoMaquinaDeEscrever.js
+++ js/animacaoMaquinaDeEscrever.js
@@ -2,7 +2,7 @@
     Preciso busca no html o elemente h1 (Rodrigo Pedroso), criar uma animação em js estilo uma maquina de escrever.
 */
 
-const textoPrincipal = document.querySelector("#textoPrincipal");
+/* const textoPrincipal = document.querySelector("#textoPrincipal");
 
 const animacaoMaquinaDeEscrever = (element, texto) => {
   const listaCaracter = texto.split("");
@@ -17,3 +17,49 @@ const animacaoMaquinaDeEscrever = (element, texto) => {
 };
 
 animacaoMaquinaDeEscrever(textoPrincipal, "Rodrigo Pedroso");
+ */
+
+/* const textoPrincipal = document.querySelector("#textoPrincipal");
+const texto = "Rodrigo Pedroso";
+
+const animacaoMaquinaDeEscrever = (element, texto) => {
+  let i = 0;
+  const tempoDigitacao = 150; // Tempo em milissegundos entre cada caractere
+
+  const escreverCaractere = () => {
+    if (i < texto.length) {
+      element.innerHTML += texto.charAt(i);
+      i++;
+      setTimeout(escreverCaractere, tempoDigitacao);
+    }
+  };
+
+  escreverCaractere();
+};
+
+animacaoMaquinaDeEscrever(textoPrincipal, texto); */
+
+const textoPrincipal = document.querySelector("#textoPrincipal");
+const texto = "Rodrigo Pedroso";
+
+const animacaoMaquinaDeEscrever = (element, texto) => {
+  let i = 0;
+  const tempoDigitacao = 90; // Tempo em milissegundos entre cada caractere
+
+  const escreverCaractere = () => {
+    if (i < texto.length) {
+      element.innerHTML += texto.charAt(i);
+      i++;
+      setTimeout(escreverCaractere, tempoDigitacao);
+    } else {
+      element.innerHTML = texto; // Exibir o texto completo após a animação
+    }
+  };
+
+  escreverCaractere();
+};
+
+// Limpar o texto inicial antes de iniciar a animação
+textoPrincipal.innerHTML = "";
+
+animacaoMaquinaDeEscrever(textoPrincipal, texto);
